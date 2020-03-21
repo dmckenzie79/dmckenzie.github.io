ef32504ee5a88ec4f522a308b6061ed01c011906
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-03-21 07:48:27 -0400

    update

--- js/buttons.js
+++ js/buttons.js
@@ -1,9 +1,22 @@
+/*
+============================================
+; Title:  buttons.js
+; Author: Diandra McKenzie
+; Date:   21 March 2020
+; Description: Programs for button toggles and slides
+;===========================================
+*/
+
+//start program
+
+//More About Me Button
 $(document).ready(function() {
     $('#aboutButton').click(function() {
       $('#about-Me').slideToggle(1000);
     });
   });
 
+  //Contact Form Button
 $(document).ready(function() {
 $('#formButton').click(function() {
     $('#contact-form').slideToggle(1000);
@@ -11,8 +24,11 @@ $('#formButton').click(function() {
     });
 });
 
+//Form Submit Button
 $(document).ready(function() {
     $('#submit').click(function() {
     $('#contact-form').slideUp(1000);
     });
 });
+
+//end program
